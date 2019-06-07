fe7e1b0e922f79f04a4be8d5dcdc58646b7802ca
obdulia <obdulia.losantos@gmail.com>
2019-06-07 11:30:51 +0200

    fix: taken from aether-bootstrap

--- gateway-manager/src/decode_token.py
+++ gateway-manager/src/decode_token.py
@@ -22,18 +22,37 @@ import json
 import jwt
 import sys
 
+from helpers import request
+from settings import BASE_HOST
 
-def decode_jwt(encoded):
-    # the jwt we get from the middleware isn't encrypted or signed
-    return jwt.decode(encoded, verify=False)
+
+def check_jwt(token):
+    tokeninfo = jwt.decode(token, verify=False)
+    print_json(tokeninfo)
+
+    iss_url = tokeninfo['iss']
+    if not iss_url.startswith(BASE_HOST):
+        raise RuntimeError(f'This token does not belong to our host {BASE_HOST}')
+
+    # go to iss
+    realminfo = request(method='get', url=iss_url)
+    print_json(realminfo)
+
+    # if this call fails the token is not longer valid
+    userinfo = request(
+        method='get',
+        url=realminfo['token-service'] + '/userinfo',
+        headers={'Authorization': '{} {}'.format(tokeninfo['typ'], token)},
+    )
+    print_json(userinfo)
 
 
 def print_json(data):
     print(json.dumps(data, indent=2))
+    print('---------------------------------------')
 
 
 if __name__ == '__main__':
     token = sys.argv[1]
 
-    userinfo = decode_jwt(token)
-    print_json(userinfo)
+    check_jwt(token)
--- gateway-manager/src/register_app.py
+++ gateway-manager/src/register_app.py
@@ -18,8 +18,20 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import sys
+
 from helpers import request
-from settings import BASE_HOST, KONG_URL, KEYCLOAK_INTERNAL, MINIO_INTERNAL
+from settings import BASE_HOST, KONG_URL
+
+DATA_CORS = {
+    'name': 'cors',
+    'config.credentials': 'true',
+    'config.exposed_headers': 'Authorization',
+    'config.headers': 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, Authorization',
+    'config.max_age': 3600,
+    'config.methods': ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    'config.origins': f'{BASE_HOST}/*',
+}
 
 
 def register_app(name, url):
@@ -34,22 +46,13 @@ def register_app(name, url):
 
     # ADD CORS Plugin to Kong for whole domain CORS
     PLUGIN_URL = f'{KONG_URL}/services/{name}/plugins'
-    data_cors = {
-        'name': 'cors',
-        'config.credentials': 'true',
-        'config.exposed_headers': 'Authorization',
-        'config.headers': 'Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, Authorization',
-        'config.max_age': 3600,
-        'config.methods': ['HEAD', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-        'config.origins': f'{BASE_HOST}/*',
-    }
-    request(method='post', url=PLUGIN_URL, data=data_cors)
+    request(method='post', url=PLUGIN_URL, data=DATA_CORS)
 
     # Routes
     # Add a route which we will NOT protect
     ROUTE_URL = f'{KONG_URL}/services/{name}/routes'
     data_route = {
-        'paths' : [f'/{name}'],
+        'paths': [f'/{name}'],
         'strip_path': 'false',
         'preserve_host': 'false',  # This is keycloak specific.
     }
@@ -59,13 +62,11 @@ def register_app(name, url):
 
 
 if __name__ == '__main__':
-    # add keycloak and minio
-    services = [
-        ('keycloak', KEYCLOAK_INTERNAL),
-        ('minio', MINIO_INTERNAL),
-    ]
+    # add service
+    name = sys.argv[1]
+    url = sys.argv[2]
 
-    for name, url in services:
-        print(f'Exposing service "{name}" @ {url}')
-        register_app(name, url)
-        print(f'Service "{name}" @ {url} now being served by kong @ {BASE_HOST}/{name}')
+    print(f'Exposing service "{name}" @ {url}')
+    _id = register_app(name, url)
+    print(f'Service "{name}" @ {url} now being served by kong @ {BASE_HOST}/{name}')
+    print(f'Service id: {_id}')
