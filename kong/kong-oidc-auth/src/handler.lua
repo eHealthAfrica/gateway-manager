6cfbb350947f99f59ad703c97ff4ab294993fed3
Obdulia Losantos <obdulia.losantos@ehealthnigeria.org>
2021-02-09 17:33:47 +0100

    fix(kong): lua style guide (#71)
    https://github.com/luarocks/lua-style-guide

--- kong/kong-oidc-auth/src/handler.lua
+++ kong/kong-oidc-auth/src/handler.lua
@@ -1,15 +1,15 @@
 local BasePlugin = require "kong.plugins.base_plugin"
-local access     = require "kong.plugins.kong-oidc-auth.access"
+local access = require "kong.plugins.kong-oidc-auth.access"
 
 local KongOidcAuth = BasePlugin:extend()
 
 function KongOidcAuth:new()
-  KongOidcAuth.super.new(self, "kong-oidc-auth")
+   KongOidcAuth.super.new(self, "kong-oidc-auth")
 end
 
 function KongOidcAuth:access(conf)
-  KongOidcAuth.super.access(self)
-  access.run(conf)
+   KongOidcAuth.super.access(self)
+   access.run(conf)
 end
 
 return KongOidcAuth
